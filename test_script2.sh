518a4ee205784516895d0701ffa787c9b6233f96
ankursheth <sankur91@gmail.com>
2017-12-03 21:00:02 -0800

    Q2_First_Script-# For each file in the $dir_path, append the name of the file with the current time-stamp

--- test_script2.sh
+++ test_script2.sh
@@ -1,3 +1,5 @@
+#script for the First Problem of Scripting Option
+
 #!/bin/bash
 
 # If less than one parmeter is provided to the shell script, give an error message
@@ -9,13 +11,12 @@ if (( $# < 1 )); then
 fi
 
 dir_path=$1
-echo ${dir_path}
+# echo ${dir_path}
 
 # For each file in the $dir_path, append the name of the file with the current
 # timestamp.
 for entry in `ls ${dir_path}`; do
-    echo ${entry}
+    # echo ${entry}
     file_path=${dir_path}/${entry}
     mv $file_path ${file_path}_$(date -d "today" +"%Y%m%d%H%M") 
 done
-
